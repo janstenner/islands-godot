fa3521ce2b9dc6917855dcae49c4ac2f9c5135a0
Jan Stenner <jan.stenner@uni-paderborn.de>
2024-04-29 11:28:20 +0200

    some preparations to the hammer thing

---
+++ scriptzzz/state.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name State
+
+# from https://www.youtube.com/watch?v=i0Y6anqiJ-g
+
+signal state_transition
+
+func Enter():
+	pass
+	
+func Exit():
+	pass
+	
+func Update(_delta:float):
+	pass
