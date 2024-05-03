f9704c5fca5014cd2f0d600630ecc87410bc0c3b
Alexander Wakup <alexander.wakup@thenativeweb.io>
2024-05-03 10:48:37 +0200

    add grass

--- scriptzzz/world.gd
+++ scriptzzz/world.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var noise_height_texture : NoiseTexture2D
+@export var grassThreshold : float = 0.3
+
 var noise : Noise
 
 var width : int = 160
@@ -9,10 +11,12 @@ var height : int = 160
 @onready var tile_map = $TileMap
 @onready var player = $Player
 
+
 var source_id = 2
 var outer_atlas = Vector2i(0,2)
 var water_atlas = Vector2i(0,1)
-var land_atlas = Vector2i(0,0)
+var sand_atlas = Vector2i(0,0)
+var grass_atlas = Vector2i(1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -43,7 +47,10 @@ func generateWorld(origin_x, origin_y):
 			tile_map.set_cell(0, Vector2i(x,y), source_id, water_atlas)
 			
 			var noise_val : float = noise.get_noise_2d(x,y)
-			if noise_val >= 0.0:
-				tile_map.set_cell(1, Vector2i(x,y), source_id, land_atlas)
-				
+			
+			# set sand or grass tiles depending on threshold
+			if noise_val >= 0.0 && noise_val <= grassThreshold:
+				tile_map.set_cell(1, Vector2i(x,y), source_id, sand_atlas)
+			elif noise_val > grassThreshold:
+				tile_map.set_cell(1, Vector2i(x,y), source_id, grass_atlas)
 
